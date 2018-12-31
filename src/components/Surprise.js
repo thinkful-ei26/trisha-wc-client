6b77b422594760868a5705ff7721f7ad88246bf9
kronicle114 <trisha.aguinaldo@gmail.com>
2018-12-31 08:30:49 -0500

    pseudo-code on surprise

--- src/components/Surprise.js
+++ src/components/Surprise.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Surprise(props) {
 
-  //1. On click, do an async action to fetch a random recipe from db
+  //1. On click, do an async action to fetch one random recipe from db
   // refer to RecipeList, actions/index.js, and recipeReducer
   //2. .then render the recipe (replace the whole RecipeList with a SurpriseRecipe Component)
 
