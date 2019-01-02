18a8e07136a6d79a5780e08fede263150347ecb3
kronicle114 <trisha.aguinaldo@gmail.com>
2019-01-02 11:39:09 -0500

    added info

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,11 @@
 import React from 'react';
 import TopNav from './TopNav';
-// import InfoModal from './InfoModal';
-// import RecipeForm from './RecipeForm';
 
 export default function Header(props) {
 
-  // if(props.info === true) {
-  //   return (
-  //   < InfoModal info={props.info} handleGotItClick={ props.handleGotItClick}/>)
-  // }
-
-  // if(props.addRecipe === true) {
-  //   return (
-  //     < RecipeForm 
-  //       // addRecipe={props.addRecipe} 
-  //       // handleCancelClick={ props.handleCancelClick}
-  //     />
-  //   )
-  // }
-    
   return (
     <header>
-      <TopNav 
-      // info={props.info}
-      // handleInfoClick={props.handleInfoClick}
-      // handleAddRecipe={props.handleAddRecipe}
-      />
+      <TopNav />
       <h1 className="whats-cooking-header ">What's Cooking?</h1>
     </header>
   );
--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -9,18 +9,18 @@ export function InfoModal(props) {
         <div className="info-content">
           <h3>How to use this app</h3>
           <div>
-            <p>Bacon ipsum dolor amet alcatra chicken filet mignon beef. Pork belly turkey tri-tip pork boudin chicken ribeye, cow kevin beef ribs tongue cupim sausage ground round. Bresaola venison andouille shankle doner, ball tip prosciutto buffalo beef fatback meatball pork loin. Tri-tip salami bresaola beef ribs ham hock. Fatback sirloin brisket, pig hamburger turkey landjaeger prosciutto beef capicola pork loin cow. Turducken chuck rump kevin tri-tip, beef ribs cow venison doner strip steak leberkas swine pork pig.</p>
+            <p>Browse through the recipe list for your meal ideas. Click the recipe title to show detailed view, including ingredients and directions.</p>
             <p>
-            Corned beef cow ground round chuck. Spare ribs pork belly jerky pastrami flank jowl, pork loin landjaeger cow. Leberkas cupim kevin tenderloin beef ribs. Turducken prosciutto swine pork belly corned beef, chuck beef ribs. Swine doner cow, short ribs ribeye pork loin brisket bresaola rump boudin ground round spare ribs landjaeger picanha salami.
+            Still unsure what to cook? Click the "Surprise Me" button for a random recipe. 
             </p>
-            {/* <a 
-            className="close" href="#info"
-            onClick={ () => props.dispatch(gotIt()) }
-            >Got It!</a> */}
-             <button 
+            
+            <button 
             className="close"
             onClick={ () => props.dispatch(gotIt()) }
-            >Got It!</button>
+            >
+              Got It!
+            </button>
+
           </div>
         </div>
       </div>
