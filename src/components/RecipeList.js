077b126617630732b88c7a39c3dce8fb7a54eb7f
kronicle114 <trisha.aguinaldo@gmail.com>
2019-01-05 09:05:54 -0500

    bugs on state

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -5,14 +5,12 @@ import Recipes from './Recipes';
 import { surpriseClick } from '../actions/controls';
 import fetchRecipes from '../actions';
 import './controls.css';
-// import SearchSection from './SearchSection';
 
 export class RecipeList extends React.Component {
 
   render () {
     return (
       <div className="container">
-        {/* <SearchSection /> */}
         <section className="controls">
           <button 
             className="surprise-btn"
