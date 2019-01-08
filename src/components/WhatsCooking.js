7c88ac7b2749a20cdf75d5f704036c6470f75440
kronicle114 <trisha.aguinaldo@gmail.com>
2019-01-08 15:54:11 -0500

    loading on first visit

--- src/components/WhatsCooking.js
+++ src/components/WhatsCooking.js
@@ -3,32 +3,39 @@ import './WhatsCooking.css';
 import RecipeList from './RecipeList';
 import InfoModal from './InfoModal';
 import TopNav from './TopNav';
+import { gotIt } from '../actions/nav';
 
 export class WhatsCooking extends Component {
   
   constructor(props) {
     super(props);
     this.state = {
-      firstVisit: false
+      visited: false
     }
   }
   // //check if the item exist in local storage, if it does, set state
   componentWillMount(){
-    localStorage.getItem('recipes') && this.setState({
-      firstVisit: true
-    })
+    //this will only log if the visited item on storage does not exists, first time 
+    if(!localStorage.getItem('visited')) {
+      console.log('have you visited before?');
+      this.setState({
+        visited: true
+      })
+    } else {
+      this.setState({
+        visited: true
+      })
+    }
+    // localStorage.getItem('recipes') && this.setState({
+    //   visited: true
+    // })
   }
-
-  // componentWillUpdate(){
-  //   localStorage.setItem('recipes', 'stuff')
-  // }
 
   //happens after WillMount
   componentDidMount(){
-    if(!localStorage.getItem('recipes')) {
-      // this.fetchData();
+    if(!localStorage.getItem('visited')) {
       this.setState({
-        firstVisit: false
+        visited: false
       })
     } else {
       console.log('page was loaded...welcome first timers!')
@@ -36,50 +43,26 @@ export class WhatsCooking extends Component {
   }
 
   componentWillUpdate(nextProps, nextState) {
-    localStorage.setItem('recipes', 'stuff')
+    localStorage.setItem('visited', 'stuff')
   }
 
-  // fetchData() {
-
-  // }
-
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     firstVisit: false
-  //   }
-  // }
-  // // //check if the item exist in local storage
-  // componentWillMount(){
-  //   localStorage.getItem('recipes') && this.setState({
-  //     firstVisit: true
-  //   })
-  // }
-
-  // // componentWillUpdate(){
-  // //   localStorage.setItem('recipes', 'stuff')
-  // // }
-
-  // componentDidMount(){
-  //   localStorage.setItem('recipes', 'stuff')
-  // }
-
   render() {
-    if(!this.props.firstVisit) {
+    console.log('state',this.state);
+    console.log('this.props', this.props);
+    if(!this.state.visited) {
      return (<main className="whats-cooking">
         <InfoModal />
-        <TopNav />
         <RecipeList />
       </main>)
     }
-
-    return (
-      <main className="whats-cooking">
-        <TopNav />
-        <RecipeList />
-      </main>
-    );
-  
+     if(this.state.visited) {
+      return (
+        <main className="whats-cooking">
+          <TopNav />
+          <RecipeList />
+        </main>
+      );
+    }  
   }
 }
 
