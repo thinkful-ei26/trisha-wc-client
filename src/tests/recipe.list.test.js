b3ed50df41f3d32cced1e526986b933d06a1005b
kronicle114 <trisha.aguinaldo@gmail.com>
2019-01-10 14:24:49 -0500

    working on recipelist & recipe form tests

--- src/tests/recipe.list.test.js
+++ src/tests/recipe.list.test.js
@@ -1,49 +1,71 @@
 import React from 'react';
-import {shallow, mount} from 'enzyme';
+import {shallow} from 'enzyme';
 import RecipeList from '../components/RecipeList';
 
-describe('<RecipeList />', () => {
-  const recipes = [ 
-    { ing: [ 'ing 1','ing 2','ing 3'],
-      expanded: false,
-      title: 'Recipe Title 1',
-      desc: 'Bacon ipsum.',
-      serving: '2',
-      imgUrl:'https://sweettootsco.files.wordpress.com/2018/12/calzone.jpg',
-      prep: '10 min',
-      cook: '20 min',
-      directions:'Bacon ipsum',
-      id: '00001' 
-    },
-    { ing: [ 'ing 1','ing 2','ing 3'],
-      expanded: false,
-      title: 'Recipe Title 2',
-      desc: 'Bacon ipsum.',
-      serving: '2',
-      imgUrl:'https://sweettootsco.files.wordpress.com/2018/12/calzone.jpg',
-      prep: '10 min',
-      cook: '20 min',
-      directions:'Bacon ipsum',
-      id: '00002' 
-    },
-  ];
+const mockFetchRecipeAction = {
+  type: 'FETCH RECIPE'
+};
+jest.mock('../actions', () => Object.assign({}, require.requireActual('../actions')), {
+  fetchRecipe: jest.fn().mockImplementation(() => {
+    return mockFetchRecipeAction;
+  })
+});
 
-  it('Renders without crashing', () => {
-    shallow(<RecipeList error={null} loading={false} recipes={recipes} />)
+const recipes = [ 
+  { ing: [ 'ing 1','ing 2','ing 3'],
+    expanded: false,
+    title: 'Recipe Title 1',
+    desc: 'Bacon ipsum.',
+    serving: '2',
+    imgUrl:'https://sweettootsco.files.wordpress.com/2018/12/calzone.jpg',
+    prep: '10 min',
+    cook: '20 min',
+    directions:'Bacon ipsum',
+    id: '00001' 
+  },
+  { ing: [ 'ing 1','ing 2','ing 3'],
+    expanded: false,
+    title: 'Recipe Title 2',
+    desc: 'Bacon ipsum.',
+    serving: '2',
+    imgUrl:'https://sweettootsco.files.wordpress.com/2018/12/calzone.jpg',
+    prep: '10 min',
+    cook: '20 min',
+    directions:'Bacon ipsum',
+    id: '00002' 
+  },
+];
+
+describe('<RecipeList />', () => {
+  let seedList = [];
+  beforeAll(() => {
+    for (let i=0; i<10; i++) {
+      seedList.push({
+        recipes
+      })
+    }
   })
 
-  // it('Renders contents initially', () => {
-  //   const wrapper = shallow(<RecipeList error={null} loading={false} recipes={recipes} />)
-  
-  //   expect(wrapper.hasClass('ing-list'));
-  //   expect(wrapper.hasClass('askdjha'))
-  //   expect(wrapper.contains(<a
-  //     className="github-repo" href="https://github.com/thinkful-ei26/trisha-wc-client"
-  // >
-  //   GitHub Repo
-  // </a>))
+  it('Renders without crashing', () => {
+    const dispatch = jest.fn();
+    shallow(<RecipeList error={null} loading={false} recipes={recipes} dispatch={dispatch} />)
+  })
 
-  //end of it test
+  // it('Dispatches fetchBoard on mount', () => {
+  //   const dispatch = jest.fn();
+  //   shallow(<RecipeList error={null} loading={false} recipes={recipes} dispatch={dispatch} />);
+  //   expect(dispatch).toHaveBeenCalledWith(mockFetchRecipeAction);
+  // });
+
+  // it('Renders contents initially', () => {
+  //   const dispatch = jest.fn();
+  //   const wrapper = mount(<RecipeList error={null} loading={false} recipes={recipes} dispatch={dispatch}/>)
+
+  //   expect(wrapper.find('.controls')).exists().toEqual(true)
+  // });
+
+  // it('Should dispatch fetchRecipesSuccess', () => {
+   
   // });
 
 //end of recipes test   
